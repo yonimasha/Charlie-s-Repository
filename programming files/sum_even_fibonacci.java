1efa6d83b6421788a176b4be17214ae38e4c58ed
adimento <98419052+adimento@users.noreply.github.com>
2023-03-21 17:28:45 -0400

    fixed if statement so the while loop is not infinite

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -4,7 +4,7 @@ public class sum_even_fibonacci {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b > 4000000) {
-            if (b % 2 == 1) {
+            if (b % 2 == 0) {
                 sumEven += b;
             }
             int temp = b;
