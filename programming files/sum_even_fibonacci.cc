0ade4517b7d016c7f457c54b1ad95adcffe76c78
yonimasha <111796689+yonimasha@users.noreply.github.com>
2023-03-21 16:58:52 -0400

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,15 +1,16 @@
+// bugs introduced: YM
 #include <iostream>
 
 int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
-    while (b < 4000000) {
-        if (b % 2 == 0) {
+    while (b > 4000000) {
+        if (b % 2 == 1) {
             sum_even += b;
         }
         int temp = b;
-        b = a + b;
+        b = a - b;
         a = temp;
     }
     std::cout << sum_even << std::endl;
