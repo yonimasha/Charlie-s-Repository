b1038e1ba1269a2779ec2b3b9eab67aac988501d
adimento <98419052+adimento@users.noreply.github.com>
2023-03-21 16:55:15 -0400

    added 2 bugs

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -1,11 +1,11 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+//bugs introduced by DD
 public class datetime_convert {
     public static void main(String[] args) {
         
         // String that contains the date and time
-        String dateStr = "2022-03-17 10:45:30";
+        String dateStr = "2022-03-17 10:45:30"
         
         // Creates a formatter that will be used later
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -16,6 +16,6 @@ public class datetime_convert {
         // Formatting the date and setting a varialbe equal to the result of the formatting
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
-        System.out.println(formattedDate);
+        System.out.println(fomattedDate);
     }
 }
