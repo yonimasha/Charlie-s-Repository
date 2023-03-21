3e57d5c0837e4d08eee53ee034dc81ebb228c59d
Charlie-Corriero <112521858+Charlie-Corriero@users.noreply.github.com>
2023-03-21 16:51:48 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -4,11 +4,15 @@
 def rgb_to_hex(r, g, b):
     
     # get rgb values in hex form
+    # The 'max' should be 'min'
     r = max(0, max(255, r))
     g = max(0, min(255, g))
+    
+    #g is supposed to be r
     b = max(0, min(255, g))
     
     # return hex string
+    #g and r are swapped
     return '{:02X}{:02X}{:02X}'.format(g, r, b)
 
 
